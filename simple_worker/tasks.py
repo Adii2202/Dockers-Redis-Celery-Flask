95f51886a8394ae3557043b309f88244f063c0ac
Aditya Ningule <adiningule2@gmail.com>
2024-02-19 14:01:11 +0530

    bugs fixed

--- simple_worker/tasks.py
+++ simple_worker/tasks.py
@@ -2,14 +2,14 @@ import time
 from celery import Celery
 from celery.utils.log import get_task_logger
 
-
 logger = get_task_logger(__name__)
 
-app = Celery("tasks", broker="redis://redis:6379/0", backend="redis://redis:6379/0")
+app = Celery('tasks', broker='redis://redis:6379/0', backend='redis://redis:6379/0')
+
 
 @app.task()
 def longtime_add(x, y):
-    logger.info('Got Request - Starting work')
+    logger.info('Got Request - Starting work ')
     time.sleep(4)
-    logger.info("Work finished")
-    return x + y  
+    logger.info('Work Finished ')
+    return x + y
